3dc9009496a6423d27c93099a1f310e484f0a8f9
Frank Gill <fgill@apparatus.net>
2022-09-05 13:03:57 -0500

    Initial commit of files for Iowa Presentations

---
+++ CreateMaintenanceSolution.ps1
@@ -0,0 +1,39 @@
+﻿$instancename = 'localhost';
+$fullschedule = 'Ola Weekly Full - Sunday 01:00';
+$diffschedule = 'Ola Daily Diff - 01:00 Except Sunday'
+$logschedule = 'Ola Daily Log - Every 15 Minutes';
+$fulljob = 'DatabaseBackup - USER_DATABASES - FULL'
+$diffjob = 'DatabaseBackup - USER_DATABASES - DIFF'
+$logjob = 'DatabaseBackup - USER_DATABASES - LOG'
+$fullsplat = @{
+    SqlInstance = $instancename
+    Schedule = $fullschedule
+    FrequencyType = 'Weekly'
+    FrequencyInterval = 'Sunday'
+    StartTime = '010000'
+    Job = $fulljob
+    #FrequencyRecurrencyFactor = 1
+    Force = $true
+}
+$diffsplat = @{
+    SqlInstance = $instancename
+    Schedule = $diffschedule
+    FrequencyType = 'Weekly'
+    StartTime = '010000'
+    Job = $diffjob
+    Force = $true
+}
+$logsplat = @{
+    SqlInstance = $instancename
+    Schedule = $logschedule
+    FrequencyType = 'Daily'
+    FrequencySubdayType = 'Minutes'
+    FrequencySubdayInterval = 15
+    Job = $logjob
+    Force = $true
+}
+New-DbaAgentSchedule @fullsplat;
+New-DbaAgentSchedule @diffsplat -FrequencyInterval Weekdays, Saturday;
+New-DbaAgentSchedule @logsplat;
+
+
